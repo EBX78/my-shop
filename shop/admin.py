19f48c697ba9968df63ea331b01803a88a9e92f6
Abdullah Amiri <abdullah.7901741@gmail.com>
2022-12-25 13:46:17 +0330

    modify db and cache again

--- shop/admin.py
+++ shop/admin.py
@@ -9,7 +9,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author', 'slug', 'fprice', 'in_stock', 'is_active', 'fcreated', 'fupdated']
+    list_display = ['title', 'slug', 'fprice', 'in_stock', 'is_active', 'fcreated', 'fupdated', 'category']
     list_filter = ['in_stock', 'is_active']
-    list_editable = ['in_stock', 'is_active']
+    # list_editable = ['in_stock', 'is_active']
     prepopulated_fields = {'slug':('title',)}
